b7f9a898e806fb51d7c3943b8975bccc3b5ff2f9
it24103583 <it24103583@my.sliit.lk>
2024-10-03 23:46:09 +0530

    Add files via upload

---
+++ Q2/IT24103583Lab7Q2A.java
@@ -0,0 +1,12 @@
+public class IT24103583Lab7Q2A{
+public static void main(String[] args){
+int i,j;
+	for (i=1;i<=4;i++){
+		for(j=1;j<=5;j++){
+	System.out.print("$ ");
+}
+	System.out.print("\n");
+}
+
+}
+}
